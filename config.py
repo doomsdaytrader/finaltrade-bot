0f7249bec059de066ce37dab76aad906edbeb824
doomsdaytrader <irayecrypto@gmail.com>
2026-03-12 20:55:33 -0500

    Fix config.py - use os.environ directly for Render

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN_HERE")
-GROUP_ID = os.getenv("GROUP_ID", "-100YOUR_GROUP_ID")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+GROUP_ID = os.environ.get("GROUP_ID", "")
 
 # Topic IDs - User will update these once topics are created
-TOPIC_MARKET = os.getenv("TOPIC_MARKET", "0")
-TOPIC_SIGNALS = os.getenv("TOPIC_SIGNALS", "0")
-TOPIC_NEWS = os.getenv("TOPIC_NEWS", "0")
-TOPIC_SURVIVAL = os.getenv("TOPIC_SURVIVAL", "0")
+TOPIC_MARKET = os.environ.get("TOPIC_MARKET", "0")
+TOPIC_SIGNALS = os.environ.get("TOPIC_SIGNALS", "0")
+TOPIC_NEWS = os.environ.get("TOPIC_NEWS", "0")
+TOPIC_SURVIVAL = os.environ.get("TOPIC_SURVIVAL", "0")
