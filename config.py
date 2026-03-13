72b3841a222a361f006b34ccd5a12f6e7b3940ad
doomsdaytrader <irayecrypto@gmail.com>
2026-03-12 21:33:21 -0500

    Add TOPIC_SIGNALS config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 GROUP_ID = os.environ.get("GROUP_ID", "")
 
-# Topic IDs - User will update these once topics are created
+# Topic IDs - Set these after creating topics in your Telegram Supergroup
 TOPIC_MARKET = os.environ.get("TOPIC_MARKET", "0")
 TOPIC_SIGNALS = os.environ.get("TOPIC_SIGNALS", "0")
 TOPIC_NEWS = os.environ.get("TOPIC_NEWS", "0")
